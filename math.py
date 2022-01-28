7b2bfe8e18f6705fb9020956bacfc6b771b7e026
Nikita-Yadav2000 <98536030+Nikita-Yadav2000@users.noreply.github.com>
2022-01-28 16:52:19 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,22 @@
+# add implementation
+def add(x,y)
+	return x+y
+    
+#subtract implementation	
+def subtract(x,y)
+	return x-y       # on master 
+#multiply implementation		
+def multiply(x,y)
+	x*y              #on bug456
+    
+#divide implementation
+def divide(x,y)           #on master branch
+	if y==0
+        return DIVIDE_BY_0_ERROR
+    else
+        return x/y
+    
+#implementation square
+def sq(x):
+pass
+    
